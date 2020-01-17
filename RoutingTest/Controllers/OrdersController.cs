9be4ed81539e122ee7ee35924e1fe1b9e3a6e6d4
Dan Ports <dan@codewithin.com>
2020-01-16 21:38:56 -0500

    Initial import.

---
+++ RoutingTest/Controllers/OrdersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingTest.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(id);
+        }
+    }
+}
